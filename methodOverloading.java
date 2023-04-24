a97f39a6edba0a57fba78d355d43997ac0dbfdba
kshitij dwivedi <kshitij.dwi.0604@gmail.com>
2023-04-25 01:43:21 +0530

    Method Overloading basics

---
+++ methodOverloading.java
@@ -0,0 +1,35 @@
+class calc{
+    int a;
+    int b;
+    public int add(int a,int b){
+        int result=a+b;
+        return result;
+    }
+    public float add(float a,float b){
+        float result=a+b;
+        return result;
+    }
+    public int add(int a,int b,int c){
+        int result=a+b+c;
+        return result;
+    }
+}
+public class methodOverloading {
+    public static void main(String[] args) {
+        
+        calc obj1 = new calc();
+        int result= obj1.add(12,20);
+        System.out.println(result);
+
+        calc obj2 = new calc();
+        int result2=obj2.add(12,34,65);
+        System.out.println(result2);
+
+        calc obj3 = new calc();
+        float result3=obj3.add(1.2f,2.2f);
+        System.out.println(result3);
+
+    }
+
+
+}
