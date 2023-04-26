ad99539b57c97910820cc2e19fa82b01bbf50413
kshitij dwivedi <kshitij.dwi.0604@gmail.com>
2023-04-26 18:01:24 +0530

    added sout statements

--- diff.java
+++ diff.java
@@ -1,5 +1,13 @@
 public class diff {
     public static void main(String[] args) {
-        
+        int a=10;
+        double d=7.45;
+        int c=a+(int)d;
+        double e=a+d;
+        double f=a+d;
+
+        System.out.println(c);
+        System.out.println(e);
+        System.out.println(f);
     }
 }
